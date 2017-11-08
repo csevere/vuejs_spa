a121f516c39545b92f59332b66fe9f844a4bc228
Labstation 3 <labstation3@local.flagco.com>
2017-11-08 16:38:43 -0500

    added routes

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
-
+import Hello from './components/Hello'
+import About from './components/About'
 //tell vue to use the router
 Vue.use(VueRouter)
 //define your routes
-const routes = []
+const routes = [
+  {path: '/', component: Hello},
+  {path: '/about', component: About}
+]
 
 
 // Create the router instance and pass the `routes` option
@@ -18,15 +22,18 @@ const router = new VueRouter({
   mode: 'history'
 })
 
-
-
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+//instatiate the vue instance
 new Vue({
+  //define the selector for the root component
   el: '#app',
+  //pass in the router to the Vue instance  
   router,
+  //pass the template to the root component
   template: '<App/>',
-  components: { App }
-})
+  //declare components that the root component can access
+  components: { App },
+}).$mount('#app') // mount the router on the app 
